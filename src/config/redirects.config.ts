69092a492b7a53e9df758698a7fbc9d81d346ce8
Stefan Probst <stefanprobst@posteo.de>
2024-06-01 20:01:46 +0200

    fix: ensure de and en pages have same pathname

--- src/config/redirects.config.ts
+++ src/config/redirects.config.ts
@@ -45,14 +45,14 @@ export const redirects = {
 	"/acdh-ch-tool-gallery-10-2": "/en/acdh-ch-tool-gallery-10-2",
 	"/acdh-ch-tool-gallery-8-2-teaching-digital-humanities":
 		"/en/acdh-ch-tool-gallery-8-2-teaching-digital-humanities",
-	"/acdh-ch-tool-gallery-9-1-xslt-dh-2": "/en/acdh-ch-tool-gallery-9-1-xslt-dh-2",
+	"/acdh-ch-tool-gallery-9-1-xslt-dh-2": "/en/acdh-ch-tool-gallery-9-1-xslt-dh",
 	"/acdh-ch-tool-gallery-9-2-transkribus-for-beginners":
 		"/en/acdh-ch-tool-gallery-9-2-transkribus-for-beginners",
-	"/acdh-ch-tool-gallery-9-3-2": "/en/acdh-ch-tool-gallery-9-3-2",
+	"/acdh-ch-tool-gallery-9-3-2": "/en/acdh-ch-tool-gallery-9-3",
 	"/clarin-cafe-computer-assisted-pragmatic-annotation-of-native-and-learner-corpora-2":
-		"/en/clarin-cafe-computer-assisted-pragmatic-annotation-of-native-and-learner-corpora-2",
+		"/en/clarin-cafe-computer-assisted-pragmatic-annotation-of-native-and-learner-corpora",
 	"/clarin-cafe-translation-technologies-and-workflows-for-ssh-researchers-2":
-		"/en/clarin-cafe-translation-technologies-and-workflows-for-ssh-researchers-2",
+		"/en/clarin-cafe-translation-technologies-and-workflows-for-ssh-researchers",
 	"/computer-vision-for-digital-humanists-a-three-day-winter-school-hosted-by-the-university-of-graz":
 		"/en/computer-vision-for-digital-humanists-a-three-day-winter-school-hosted-by-the-university-of-graz",
 	"/conference-on-cultural-heritage-new-technologies-cultural-heritage-nextgen":
@@ -61,13 +61,13 @@ export const redirects = {
 	"/dazzling-data-lectureseries-izmf-23-24": "/en/dazzling-data-lectureseries-izmf-23-24",
 	"/dch-speed-dating": "/en/dch-speed-dating",
 	"/digedtnt-workshop-meet-the-tools-and-developers-2":
-		"/en/digedtnt-workshop-meet-the-tools-and-developers-2",
+		"/en/digedtnt-workshop-meet-the-tools-and-developers",
 	"/digital-humanities-lecture-circuit-23-24": "/en/digital-humanities-lecture-circuit-23-24",
 	"/digital-linguistics-recent-developments-in-austria-olt-2023":
 		"/en/digital-linguistics-recent-developments-in-austria-olt-2023",
 	"/digital-methods-live-seminar": "/en/digital-methods-live-seminar",
 	"/exploresalon-registrations-open": "/en/exploresalon-registrations-open",
-	"/museum_law-2023-ai-co-creation-nft": "/en/museum_law-2023-ai-co-creation-nft",
+	"/museum_law-2023-ai-co-creation-nft": "/en/museum-law-2023-ai-co-creation-nft",
 	"/projectpresentation-digital-edition-baernreither":
 		"/en/projectpresentation-digital-edition-baernreither",
 	"/ssh-open-marketplace-online-workshop-for-phd-students":
@@ -79,66 +79,66 @@ export const redirects = {
 	"/webinar-series-digital-editions-tools-transitions":
 		"/en/webinar-series-digital-editions-tools-transitions",
 	"/workshop-central-european-book-history-data-tools-2":
-		"/en/workshop-central-european-book-history-data-tools-2",
+		"/en/workshop-central-european-book-history-data-tools",
 	"/workshop-interface-of-medieval-studies": "/en/workshop-interface-of-medieval-studies",
 	"/workshop_writing-a-digital-conceptual-history-of-democracy":
-		"/en/workshop_writing-a-digital-conceptual-history-of-democracy",
+		"/en/workshop-writing-a-digital-conceptual-history-of-democracy",
 
-	// "/de/acdh-ch-lecture-10-1-2": "/de/acdh-ch-lecture-10-1-2",
-	// "/de/acdh-ch-lecture-10-2-2": "/de/acdh-ch-lecture-10-2-2",
-	// "/de/acdh-ch-lecture-10-3-2": "/de/acdh-ch-lecture-10-3-2",
-	// "/de/acdh-ch-lecture-8-1-the-problem-with-social-media-research-2":
-	// 	"/de/acdh-ch-lecture-8-1-the-problem-with-social-media-research-2",
-	// "/de/acdh-ch-lecture-8-2-the-role-of-simplicity-and-linking-in-the-practice-of-digital-archaeology-2":
-	// 	"/de/acdh-ch-lecture-8-2-the-role-of-simplicity-and-linking-in-the-practice-of-digital-archaeology-2",
-	// "/de/acdh-ch-lecture-8-3-2": "/de/acdh-ch-lecture-8-3-2",
-	// "/de/acdh-ch-lecture-8-4-contextualizing-museum-data-digitization-infrastructure-and-digital-literacy-2":
-	// 	"/de/acdh-ch-lecture-8-4-contextualizing-museum-data-digitization-infrastructure-and-digital-literacy-2",
-	// "/de/acdh-ch-lecture-9-1-1": "/de/acdh-ch-lecture-9-1-1",
-	// "/de/acdh-ch-lecture-9-2-2": "/de/acdh-ch-lecture-9-2-2",
-	// "/de/acdh-ch-lecture-9-3-2": "/de/acdh-ch-lecture-9-3-2",
-	// "/de/acdh-ch-tool-gallery-10-1-2": "/de/acdh-ch-tool-gallery-10-1-2",
-	// "/de/acdh-ch-tool-gallery-10-2-2": "/de/acdh-ch-tool-gallery-10-2-2",
-	// "/de/acdh-ch-tool-gallery-8-2-teaching-digital-humanities-2":
-	// 	"/de/acdh-ch-tool-gallery-8-2-teaching-digital-humanities-2",
+	"/de/acdh-ch-lecture-9-1-1": "/de/acdh-ch-lecture-9-1",
+	"/de/acdh-ch-lecture-9-2-2": "/de/acdh-ch-lecture-9-2",
+	"/de/acdh-ch-lecture-9-3-2": "/de/acdh-ch-lecture-9-3",
+	"/de/acdh-ch-lecture-10-1-2": "/de/acdh-ch-lecture-10-1",
+	"/de/acdh-ch-lecture-10-2-2": "/de/acdh-ch-lecture-10-2",
+	"/de/acdh-ch-lecture-10-3-2": "/de/acdh-ch-lecture-10-3",
+	"/de/acdh-ch-lecture-8-1-the-problem-with-social-media-research-2":
+		"/de/acdh-ch-lecture-8-1-the-problem-with-social-media-reserach",
+	"/de/acdh-ch-lecture-8-2-the-role-of-simplicity-and-linking-in-the-practice-of-digital-archaeology-2":
+		"/de/acdh-ch-lecture-8-2-the-role-of-simplicity-and-linking-in-the-practice-of-digital-archaeology",
+	"/de/acdh-ch-lecture-8-3-2": "/de/acdh-ch-lecture-8-3",
+	"/de/acdh-ch-lecture-8-4-contextualizing-museum-data-digitization-infrastructure-and-digital-literacy-2":
+		"/de/acdh-ch-lecture-8-4-contextualizing-museum-data-digitization-infrastructure-and-digital-literacy",
+	"/de/acdh-ch-tool-gallery-10-1-2": "/de/acdh-ch-tool-gallery-10-1",
+	"/de/acdh-ch-tool-gallery-10-2-2": "/de/acdh-ch-tool-gallery-10-2",
+	"/de/acdh-ch-tool-gallery-8-2-teaching-digital-humanities-2":
+		"/de/acdh-ch-tool-gallery-8-2-teaching-digital-humanities",
 	// "/de/acdh-ch-tool-gallery-9-1-xslt-dh": "/de/acdh-ch-tool-gallery-9-1-xslt-dh",
-	// "/de/acdh-ch-tool-gallery-9-2-transkribus-fur-anfangerinnen":
-	// 	"/de/acdh-ch-tool-gallery-9-2-transkribus-fur-anfangerinnen",
+	"/de/acdh-ch-tool-gallery-9-2-transkribus-fur-anfangerinnen":
+		"/de/acdh-ch-tool-gallery-9-2-transkribus-for-beginners",
 	// "/de/acdh-ch-tool-gallery-9-3": "/de/acdh-ch-tool-gallery-9-3",
 	// "/de/clarin-cafe-computer-assisted-pragmatic-annotation-of-native-and-learner-corpora":
 	// 	"/de/clarin-cafe-computer-assisted-pragmatic-annotation-of-native-and-learner-corpora",
 	// "/de/clarin-cafe-translation-technologies-and-workflows-for-ssh-researchers":
 	// 	"/de/clarin-cafe-translation-technologies-and-workflows-for-ssh-researchers",
-	// "/de/computer-vision-for-digital-humanists-eine-dreitagige-winterschule-organisiert-von-der-universitat-graz":
-	// 	"/de/computer-vision-for-digital-humanists-eine-dreitagige-winterschule-organisiert-von-der-universitat-graz",
-	// "/de/conference-on-cultural-heritage-new-technologies-cultural-heritage-nextgen-2":
-	// 	"/de/conference-on-cultural-heritage-new-technologies-cultural-heritage-nextgen-2",
-	// "/de/dariah-campus-how-to-workshop-2": "/de/dariah-campus-how-to-workshop-2",
-	// "/de/dazzling-data-izmf-wise23-24": "/de/dazzling-data-izmf-wise23-24",
-	// "/de/dch-speed-dating-2": "/de/dch-speed-dating-2",
+	"/de/computer-vision-for-digital-humanists-eine-dreitagige-winterschule-organisiert-von-der-universitat-graz":
+		"/de/computer-vision-for-digital-humanists-a-three-day-winter-school-hosted-by-the-university-of-graz",
+	"/de/conference-on-cultural-heritage-new-technologies-cultural-heritage-nextgen-2":
+		"/de/conference-on-cultural-heritage-new-technologies-cultural-heritage-nextgen",
+	"/de/dariah-campus-how-to-workshop-2": "/de/dariah-campus-how-to-workshop",
+	"/de/dazzling-data-izmf-wise23-24": "/de/dazzling-data-lectureseries-izmf-23-24",
+	"/de/dch-speed-dating-2": "/de/dch-speed-dating",
 	// "/de/digedtnt-workshop-meet-the-tools-and-developers":
 	// 	"/de/digedtnt-workshop-meet-the-tools-and-developers",
-	// "/de/digitale-linguistik-rezente-entwicklungen-in-osterreich-olt-2023":
-	// 	"/de/digitale-linguistik-rezente-entwicklungen-in-osterreich-olt-2023",
-	// "/de/digitale-methoden-live-seminar": "/de/digitale-methoden-live-seminar",
-	// "/de/exploresalon-anmeldung-offen": "/de/exploresalon-anmeldung-offen",
-	// "/de/museumrecht-2023-ki-co-creation-nft": "/de/museumrecht-2023-ki-co-creation-nft",
-	// "/de/projektprasentation-digitale-edition-baernreither":
-	// 	"/de/projektprasentation-digitale-edition-baernreither",
-	// "/de/ringvorlesung-digital-humanities-23-24": "/de/ringvorlesung-digital-humanities-23-24",
-	// "/de/ssh-open-marketplace-online-workshop-for-phd-students-2":
-	// 	"/de/ssh-open-marketplace-online-workshop-for-phd-students-2",
-	// "/de/summer-school-anmeldung-offen": "/de/summer-school-anmeldung-offen",
-	// "/de/train-the-trainers-urheberrecht-open-access-in-der-akademischen-lehre":
-	// 	"/de/train-the-trainers-urheberrecht-open-access-in-der-akademischen-lehre",
-	// "/de/transnational-research-infrastructure-2": "/de/transnational-research-infrastructure-2",
-	// "/de/webinarreihe-digital-editions-tools-transitions":
-	// 	"/de/webinarreihe-digital-editions-tools-transitions",
+	"/de/ringvorlesung-digital-humanities-23-24": "/de/digital-humanities-lecture-circuit-23-24",
+	"/de/digitale-linguistik-rezente-entwicklungen-in-osterreich-olt-2023":
+		"/de/digital-linguistics-recent-developments-in-austria-olt-2023",
+	"/de/digitale-methoden-live-seminar": "/de/digital-methods-live-seminar",
+	"/de/exploresalon-anmeldung-offen": "/de/exploresalon-registrations-open",
+	"/de/museumrecht-2023-ki-co-creation-nft": "/de/museum-law-2023-ai-co-creation-nft",
+	"/de/projektprasentation-digitale-edition-baernreither":
+		"/de/projectpresentation-digital-edition-baernreither",
+	"/de/ssh-open-marketplace-online-workshop-for-phd-students-2":
+		"/de/ssh-open-marketplace-online-workshop-for-phd-students",
+	"/de/summer-school-anmeldung-offen": "/de/summer-school-registrations-open",
+	"/de/train-the-trainers-urheberrecht-open-access-in-der-akademischen-lehre":
+		"/de/train-the-trainers-proprietary-rights-open-access-in-academic-teaching",
+	"/de/transnational-research-infrastructure-2": "/de/transnational-research-infrastructure",
+	"/de/webinarreihe-digital-editions-tools-transitions":
+		"/de/webinar-series-digital-editions-tools-transitions",
 	// "/de/workshop-central-european-book-history-data-tools":
 	// 	"/de/workshop-central-european-book-history-data-tools",
-	// "/de/workshop-schnittstelle-mediavistik": "/de/workshop-schnittstelle-mediavistik",
-	// "/de/workshop_writing-a-digital-conceptual-history-of-democracy-2":
-	// 	"/de/workshop_writing-a-digital-conceptual-history-of-democracy-2",
+	"/de/workshop-schnittstelle-mediavistik": "/de/workshop-interface-of-medieval-studies",
+	"/de/workshop_writing-a-digital-conceptual-history-of-democracy-2":
+		"/de/workshop-writing-a-digital-conceptual-history-of-democracy",
 
 	/** News. */
 
@@ -157,22 +157,22 @@ export const redirects = {
 	"/clarin-2023-call-for-abstracts/": "/en/clarin-2023-call-for-abstracts",
 	"/results-from-call-for-projects-announced": "/en/results-from-call-for-projects-announced",
 
-	// "/de/acdh-ch-lecture-mit-achim-rabus-ist-online":
-	// 	"/de/acdh-ch-lecture-mit-achim-rabus-ist-online",
-	// "/de/call-for-abstracts-clarin-2024-2": "/de/call-for-abstracts-clarin-2024-2",
-	// "/de/cfp-buchentwurf": "/de/cfp-buchentwurf",
-	// "/de/cfp-osterreichisches-treffen-zur-digitalen-linguistik-oelt-2023":
-	// 	"/de/cfp-osterreichisches-treffen-zur-digitalen-linguistik-oelt-2023",
-	// "/de/cfp-zuruck-und-in-die-zukunft-10-tagung-digitale-bibliothek":
-	// 	"/de/cfp-zuruck-und-in-die-zukunft-10-tagung-digitale-bibliothek",
-	// "/de/einladung-zur-teilnahme-dariah-2024": "/de/einladung-zur-teilnahme-dariah-2024",
-	// "/de/ringvorlesung-dazzling-data-aufzeichnungen-verfugbar":
-	// 	"/de/ringvorlesung-dazzling-data-aufzeichnungen-verfugbar",
-	// "/de/exploresalon-2023-save-the-date": "/de/exploresalon-2023-save-the-date",
-	// "/de/auswahlergebnis-der-projektausschreibung-veroffentlicht":
-	// 	"/de/auswahlergebnis-der-projektausschreibung-veroffentlicht",
-	// "/de/call-for-contributions-update-konde": "/de/call-for-contributions-update-konde",
-	// "/de/clarin-2023-call-for-abstracts-2": "/de/clarin-2023-call-for-abstracts-2",
+	"/de/acdh-ch-lecture-mit-achim-rabus-ist-online":
+		"/de/acdh-ch-lecture-9-1-with-achim-rabus-online",
+	"/de/call-for-abstracts-clarin-2024-2": "/de/call-for-abstracts-clarin-2024",
+	"/de/cfp-buchentwurf": "/de/cfp-book-proposal",
+	"/de/cfp-osterreichisches-treffen-zur-digitalen-linguistik-oelt-2023":
+		"/de/cfp-austrian-meeting-on-digital-linguistics-oelt-2023",
+	"/de/cfp-zuruck-und-in-die-zukunft-10-tagung-digitale-bibliothek":
+		"/de/cfp-back-and-into-the-future-10th-digital-library-meeting",
+	"/de/einladung-zur-teilnahme-dariah-2024": "/de/call-for-participants-dariah-2024",
+	"/de/ringvorlesung-dazzling-data-aufzeichnungen-verfugbar":
+		"/de/dazzling-data-lecture-circuit-now-available-online",
+	"/de/exploresalon-2023-save-the-date": "/de/exploresalon2023-save-the-date",
+	"/de/auswahlergebnis-der-projektausschreibung-veroffentlicht":
+		"/de/results-from-call-for-projects-announced",
+	"/de/call-for-contributions-update-konde": "/de/call-for-contributions-updating-konde",
+	"/de/clarin-2023-call-for-abstracts-2": "/de/clarin-2023-call-for-abstracts",
 
 	/** Projects which were already on wordpress. */
 
