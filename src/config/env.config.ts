3bc0de0890259aca37f329613ed4dee6f6b32ed9
Stefan Probst <stefanprobst@posteo.de>
2024-06-07 20:33:37 +0200

    chore: clean up

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -16,30 +16,40 @@ export const env = createEnv({
 	},
 	private(input) {
 		const Schema = v.object({
-			EMAIL_CONTACT_ADDRESS: v.optional(v.string([v.email()])),
-			EMAIL_SMTP_PORT: v.optional(v.coerce(v.number([v.integer(), v.minValue(1)]), Number)),
-			EMAIL_SMTP_SERVER: v.optional(v.string([v.minLength(1)])),
-			EMAIL_SMTP_USERNAME: v.optional(v.string([v.minLength(1)])),
-			EMAIL_SMTP_PASSWORD: v.optional(v.string([v.minLength(1)])),
-			KEYSTATIC_GITHUB_CLIENT_ID: v.optional(v.string([v.minLength(1)])),
-			KEYSTATIC_GITHUB_CLIENT_SECRET: v.optional(v.string([v.minLength(1)])),
-			KEYSTATIC_SECRET: v.optional(v.string([v.minLength(1)])),
+			EMAIL_CONTACT_ADDRESS: v.optional(v.pipe(v.string(), v.email())),
+			EMAIL_SMTP_PORT: v.optional(
+				v.pipe(v.string(), v.transform(Number), v.number(), v.integer(), v.minValue(1)),
+			),
+			EMAIL_SMTP_SERVER: v.optional(v.pipe(v.string(), v.nonEmpty())),
+			EMAIL_SMTP_USERNAME: v.optional(v.pipe(v.string(), v.nonEmpty())),
+			EMAIL_SMTP_PASSWORD: v.optional(v.pipe(v.string(), v.nonEmpty())),
+			KEYSTATIC_GITHUB_CLIENT_ID: v.optional(v.pipe(v.string(), v.nonEmpty())),
+			KEYSTATIC_GITHUB_CLIENT_SECRET: v.optional(v.pipe(v.string(), v.nonEmpty())),
+			KEYSTATIC_SECRET: v.optional(v.pipe(v.string(), v.nonEmpty())),
 		});
 		return v.parse(Schema, input);
 	},
 	public(input) {
 		const Schema = v.object({
-			PUBLIC_APP_BASE_PATH: v.optional(v.string([v.minLength(1)])),
-			PUBLIC_APP_BASE_URL: v.string([v.url()]),
+			PUBLIC_APP_BASE_PATH: v.optional(v.pipe(v.string(), v.nonEmpty())),
+			PUBLIC_APP_BASE_URL: v.pipe(v.string(), v.url()),
 			PUBLIC_BOTS: v.optional(v.picklist(["disabled", "enabled"]), "disabled"),
-			PUBLIC_GOOGLE_SITE_VERIFICATION: v.optional(v.string()),
-			PUBLIC_KEYSTATIC_GITHUB_APP_SLUG: v.optional(v.string([v.minLength(1)])),
-			PUBLIC_KEYSTATIC_GITHUB_REPO_NAME: v.optional(v.string([v.minLength(1)])),
-			PUBLIC_KEYSTATIC_GITHUB_REPO_OWNER: v.optional(v.string([v.minLength(1)])),
+			PUBLIC_GOOGLE_SITE_VERIFICATION: v.optional(v.pipe(v.string(), v.nonEmpty())),
+			PUBLIC_KEYSTATIC_GITHUB_APP_SLUG: v.optional(v.pipe(v.string(), v.nonEmpty())),
+			PUBLIC_KEYSTATIC_GITHUB_REPO_NAME: v.optional(v.pipe(v.string(), v.nonEmpty())),
+			PUBLIC_KEYSTATIC_GITHUB_REPO_OWNER: v.optional(v.pipe(v.string(), v.nonEmpty())),
 			PUBLIC_KEYSTATIC_MODE: v.optional(v.picklist(["github", "local"]), "local"),
-			PUBLIC_MATOMO_BASE_URL: v.optional(v.string([v.url()])),
-			PUBLIC_MATOMO_ID: v.optional(v.coerce(v.number([v.integer(), v.minValue(1)]), Number)),
-			PUBLIC_REDMINE_ID: v.coerce(v.number([v.integer(), v.minValue(1)]), Number),
+			PUBLIC_MATOMO_BASE_URL: v.optional(v.pipe(v.string(), v.url())),
+			PUBLIC_MATOMO_ID: v.optional(
+				v.pipe(v.string(), v.transform(Number), v.number(), v.integer(), v.minValue(1)),
+			),
+			PUBLIC_REDMINE_ID: v.pipe(
+				v.string(),
+				v.transform(Number),
+				v.number(),
+				v.integer(),
+				v.minValue(1),
+			),
 		});
 		return v.parse(Schema, input);
 	},
@@ -51,7 +61,8 @@ export const env = createEnv({
 		if (error instanceof v.ValiError) {
 			const message = "Invalid environment variables";
 
-			log.error(`${message}:`, v.flatten(error).nested);
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			log.error(`${message}:`, v.flatten<any>(error.issues).nested);
 
 			const validationError = new Error(message);
 			delete validationError.stack;
