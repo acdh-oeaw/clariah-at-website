f7f332085768a7724a4e51cf39b684d364ec5692
Stefan Probst <stefanprobst@posteo.de>
2024-06-16 13:56:54 +0200

    fix: allow gif images

--- src/lib/get-image-import.ts
+++ src/lib/get-image-import.ts
@@ -3,7 +3,9 @@ import { join } from "node:path";
 import { assert } from "@acdh-oeaw/lib";
 import type { ImageMetadata } from "astro";
 
-const images = import.meta.glob<{ default: ImageMetadata }>("/public/assets/**/*.@(jpeg|jpg|png)");
+const images = import.meta.glob<{ default: ImageMetadata }>(
+	"/public/assets/**/*.@(gif|jpeg|jpg|png|svg)",
+);
 
 export function getImageImport(path: string) {
 	/** Upstream type issue. */
