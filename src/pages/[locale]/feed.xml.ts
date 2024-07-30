e5518ddb0727fe5b95bbb6e07258aebea786cf85
Stefan Probst <stefanprobst@posteo.de>
2024-07-30 08:20:40 +0200

    fix: add link to rss feed

--- src/pages/[locale]/feed.xml.ts
+++ src/pages/[locale]/feed.xml.ts
@@ -2,13 +2,19 @@ import { createUrl } from "@acdh-oeaw/lib";
 import rss, { type RSSFeedItem } from "@astrojs/rss";
 import type { APIContext } from "astro";
 
-import { defaultLocale, type Locale } from "@/config/i18n.config";
+import { type Locale, locales } from "@/config/i18n.config";
 import { getCollectionName } from "@/lib/content/get-collection-name";
 import { reader } from "@/lib/content/reader";
 import { createI18n } from "@/lib/i18n";
 
+export function getStaticPaths() {
+	return locales.map((locale) => {
+		return { params: { locale } };
+	});
+}
+
 export async function GET(context: APIContext) {
-	const locale = (context.currentLocale as Locale | undefined) ?? defaultLocale;
+	const locale = context.params.locale as Locale;
 
 	const { t } = await createI18n(locale);
 
